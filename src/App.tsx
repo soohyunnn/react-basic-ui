4896540e5830496c71019c59307df32be82512a0
soohyunnn <sh.choi@cbcm-co.com>
2023-01-03 15:35:19 +0900

    캐러셀(Carousel)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import SkeletonPage from "./pages/SkeletonPage";
+import CarouselPage from "./pages/CarouselPage";
 
 function App() {
   return (
     <div className="App">
-      <SkeletonPage />
+      {/* <SkeletonPage /> */}
+      <CarouselPage />
     </div>
   );
 }
