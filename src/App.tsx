e6fd6f681da37a2f29d9801e07f49b9225aa70ba
soohyunnn <sh.choi@cbcm-co.com>
2023-01-03 17:00:10 +0900

    Pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import SkeletonPage from "./pages/SkeletonPage";
 import CarouselPage from "./pages/CarouselPage";
+import Pagination from "./components/Pagination";
+import axios from "axios";
+
+interface Airline {
+  id: number;
+  name: string;
+  country: string;
+  logo: string;
+  slogan: string;
+  head_quaters: string;
+  website: string;
+  established: string;
+}
+
+interface Passenger {
+  _id: string;
+  name: string;
+  trips: number;
+  airline: Airline;
+  __v: number;
+}
+
+interface Response {
+  totalPassengers: number;
+  totalPages: number;
+  data: Array<Passenger>;
+}
 
 function App() {
+  const [page, setPage] = useState<number>(0);
+
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [items, setItems] = useState<Array<Passenger>>([]);
+
+  const handlePageChange = (currentPage: number): void => {
+    setPage(currentPage);
+  };
+
+  useEffect(() => {
+    const fetch = async () => {
+      const params = { page, size: 10 };
+      const {
+        data: { totalPages, data },
+      } = await axios.get<Response>(
+        "https://api.instantwebtools.net/v1/passenger",
+        { params }
+      );
+
+      setTotalPages(totalPages);
+      setItems(data);
+    };
+
+    fetch();
+  }, [page]);
+
   return (
     <div className="App">
       {/* <SkeletonPage /> */}
-      <CarouselPage />
+      {/* <CarouselPage /> */}
+      <ul>
+        {items.map((item) => (
+          <li key={item._id}>{item.name}</li>
+        ))}
+      </ul>
+      <Pagination
+        count={totalPages}
+        page={page}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 }
