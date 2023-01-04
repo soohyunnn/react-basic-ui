5293b1b5a13003e8fa9dddff1b3ebdaa8fb120a6
soohyunnn <sh.choi@cbcm-co.com>
2023-01-04 12:51:24 +0900

    무한 스크롤 - Intersection Observer

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,102 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
-import styled from "@emotion/styled/macro";
-import { throttle } from "throttle-debounce";
+import "intersection-observer";
 
-import { Passenger, Response } from "./types";
+import useIntersectionObserver from "./hooks/observer/useIntersectionObserver";
 
-const ListItem = styled.li`
-  font-size: 36px;
-`;
+interface Airline {
+  id: number;
+  name: string;
+  country: string;
+  logo: string;
+  slogan: string;
+  head_quaters: string;
+  website: string;
+  established: string;
+}
 
-const List = styled.ul`
-  overflow-x: hidden;
-  overflow-y: scroll;
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 512px;
-  ${ListItem} + ${ListItem} {
-    margin-top: 12px;
-  }
-`;
+interface Passenger {
+  _id: string;
+  name: string;
+  trips: number;
+  airline: Airline;
+  __v: number;
+}
+
+interface Props {
+  isLastItem: boolean;
+  onFetchMorePassengers: () => void;
+  children?: React.ReactNode;
+}
+
+const Item: React.FC<Props> = ({
+  children,
+  isLastItem,
+  onFetchMorePassengers,
+}) => {
+  const ref = useRef<HTMLDivElement | null>(null); // 감시할 엘리먼트
+  const entry = useIntersectionObserver(ref, {});
+  const isIntersecting = !!entry?.isIntersecting; // 겹치는 영역이 존재하는 지 여부
+
+  useEffect(() => {
+    isLastItem && isIntersecting && onFetchMorePassengers(); // 목록의 마지막에 도달했을 때, 리스트를 더 불러오도록 요청한다.
+  }, [isLastItem, isIntersecting]);
+
+  return (
+    <div
+      ref={ref}
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        border: "1px dashed #000",
+      }}
+    >
+      <div style={{ margin: "auto" }}>{children}</div>
+    </div>
+  );
+};
 
 function App() {
-  const scrollRef = useRef<HTMLUListElement | null>(null);
-  const pageRef = useRef<number>(0);
+  const [passengers, setPassengers] = useState<Array<Passenger>>([]);
 
-  const [items, setItems] = useState<Array<Passenger>>([]);
+  const [page, setPage] = useState<number>(0);
   const [isLast, setIsLast] = useState<boolean>(false);
-  const [isScrollBottom, setIsScrollBottom] = useState<boolean>(false);
 
-  const handleScroll = throttle(1000, () => {
-    if (scrollRef.current) {
-      const { scrollHeight, offsetHeight, scrollTop } = scrollRef.current;
-
-      const offset = 50;
-
-      setIsScrollBottom(scrollHeight - offsetHeight - scrollTop < offset);
-    }
-  });
-
-  const fetch = async (init?: boolean) => {
-    const params = { size: 30, page: pageRef.current };
+  const getPassengers = async () => {
+    const params = { page, size: 30 };
 
     try {
-      const res = await axios.get<Response>(
+      const response = await axios.get(
         "https://api.instantwebtools.net/v1/passenger",
         { params }
       );
 
-      setItems(init ? res.data.data : items.concat(res.data.data));
-      setIsLast(res.data.totalPages === pageRef.current);
+      const passengers = response.data.data;
+      const isLast = response.data.totalPages === page;
+
+      setPassengers((prev) => [...prev, ...passengers]);
+      setIsLast(isLast);
     } catch (e) {
       console.error(e);
     }
   };
 
   useEffect(() => {
-    if (isScrollBottom) {
-      pageRef.current = pageRef.current + 1;
-
-      !isLast && fetch();
-    }
-  }, [isScrollBottom, isLast]);
-
-  useEffect(() => {
-    fetch(true);
-  }, []);
+    !isLast && getPassengers();
+  }, [page]);
 
   return (
-    <div>
-      <List ref={scrollRef} onScroll={handleScroll}>
-        {items.map((item) => (
-          <ListItem key={item._id}>{item.name}</ListItem>
-        ))}
-      </List>
-    </div>
+    <>
+      {passengers.map((passenger, idx) => (
+        <Item
+          key={passenger._id}
+          isLastItem={passengers.length - 1 === idx}
+          onFetchMorePassengers={() => setPage((prev) => prev + 1)}
+        >
+          {passenger.name}
+        </Item>
+      ))}
+    </>
   );
 }
 
